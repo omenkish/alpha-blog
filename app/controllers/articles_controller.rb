ac032cc1bc38548cdec3a254418aeb2a45b9434f
omenkish <omenkish@gmail.com>
2019-04-21 12:53:10 +0100

    fix error with destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :update, :show, :destory]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
@@ -37,7 +37,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
